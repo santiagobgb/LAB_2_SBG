b30ff5c48a90ce6600d4c401620df8b4a0f2328c
santiagobgb <47230298+santiagobgb@users.noreply.github.com>
2020-03-19 21:00:19 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -7,6 +7,7 @@
 
 import pandas as pd
 import numpy as np
+import math as math
 
 #%% FUNCION: leer archivo
 def f_leer_archivo(param_archivo):
@@ -105,13 +106,17 @@ def f_columnas_pips(param_data):
     
 #%% FUNCION:
 def f_estadisticas_ba(param_data):
-    df_1_tabla = pd.DataFrame(columns= 'medida', 'valor', 'descripcion')
-    medida = ['Ops totales', 'Ganadoras', 'Ganadoras_c', 'Ganadoras_v', 'Perdedoras', 'perdedoras_c', 'Perdedoras_v', 
+    
+    medida = ['Ops totales', 'Ganadoras', 'Ganadoras_c', 'Ganadoras_v', 'Perdedoras', 'Perdedoras_c', 'Perdedoras_v', 
               'Media(profit)','Media(pips)', 'r_efectividad', 'r_proporcion', 'r_efectividad_c','r_efectividad_v']
-    descripcion = ['Operaciones totales', 'operaciones ganadoras', 'operaciones ganadoras de compra', 'operaciones ganadoras de venta',
-                   'operaciones perdedoras', 'operaciones perdedoras de compra', 'operaciones perdedoras de venta', 'mediana de profit de operaciones', 
-                   'media de pips de operaciones', 'ganadoras totales / operaciones totales', 'perdedoras totales / operaciones totales',
-                   'perdedoras totales / ganadoras totales', 'ganadoras compra / operaciones totales', 'ganadoras ventas / operaciones totales']
+    descripcion = ['Operaciones Totales', 'Operaciones Ganadoras', 'Operaciones Ganadoras de Compra', 'Operaciones Ganadoras de Venta',
+                   'Operaciones Perdedoras', 'Operaciones Perdedoras de Compra', 'Operaciones Perdedoras de Venta', 'Mediana de Profit de Operaciones', 
+                   'Media de Pips de Operaciones', 'Ganadoras Totales / Operaciones Totales', 'Perdedoras Totales / Ganadoras Totales',
+                   'Ganadoras Compras / Operaciones Totales', 'Ganadoras Ventas / Operaciones Totales']
+   
+    zero_data = np.zeros(shape=(len(descripcion),3))
+    df_1_tabla = pd.DataFrame(zero_data, columns = ['medida', 'valor', 'descripcion'])
+    
     df_1_tabla['valor'][0] = len(param_data['profit'])
     df_1_tabla['valor'][1] = param_data['profit'].gt(0).sum()
     x = 0
@@ -139,18 +144,55 @@ def f_estadisticas_ba(param_data):
     df_1_tabla['valor'][6] = x
     df_1_tabla['valor'][7] = param_data.profit.median()
     df_1_tabla['valor'][8] = param_data.pips.median()
-    df_1_tabla['valor'][9] = df_1_tabla['valor'][1] / df_1_tabla['valor'][0]
-    df_1_tabla['valor'][10] = df_1_tabla['valor'][4] / df_1_tabla['valor'][1]
-    df_1_tabla['valor'][11] = df_1_tabla['valor'][2] / df_1_tabla['valor'][0] 
-    df_1_tabla['valor'][12] = df_1_tabla['valor'][3] / df_1_tabla['valor'][0] 
+    df_1_tabla['valor'][9] = df_1_tabla['valor'][0] / df_1_tabla['valor'][1]
+    df_1_tabla['valor'][10] = df_1_tabla['valor'][1] / df_1_tabla['valor'][4]
+    df_1_tabla['valor'][11] = df_1_tabla['valor'][0] / df_1_tabla['valor'][2] 
+    df_1_tabla['valor'][12] = df_1_tabla['valor'][0] / df_1_tabla['valor'][3] 
     
     for i in range(0,len(medida)):
         df_1_tabla['medida'][i] = medida[i]
         df_1_tabla['descripcion'][i] = descripcion[i]
-    
         
+    return df_1_tabla
 
 
-    
+        
+#%% FUNCIÓN:
+
+def f_columnas_capital_acum(param_data):
+    param_data['capital_acm'] = np.zeros(len(param_data['type']))
+    param_data['capital_acm'][0] = 5000+param_data['profit'][0]
+      
+    param_data['rendimiento_log'] = np.zeros(len(param_data['type']))
+    param_data['rendimiento_log'][0] = math.log(param_data['capital_acm'][0] / 5000)
+      
+    for i in range(1,len(param_data['pips'])):
+         param_data['capital_acm'][i] = param_data['capital_acm'][i-1] + param_data['profit'][i]
+         param_data['rendimiento_log'][i] = math.log(param_data['capital_acm'][i] / param_data['capital_acm'][i-1])
+
+    return param_data  
+
+#%%
+def f_estadisticas_mad(param_data):
+    rf = .08/12
+    metrica = ['sharpe', 'sortino_c','sortino_v','drawdown_capi_c','drawdown_capi_v',
+              'drawdown_pips_c','drawdown_pips_v','information_r']
+    descripcion = ['Sharpe Ratio', 'Sortino Ratio para Posiciones','Sortino Ratio para Posiciones de Venta',
+                   'DrawDown de Capital', 'DrawDown de Pips','DrawUp de Pips','Information Ratio']
    
+    zero_data = np.zeros(shape=(len(descripcion),3))
+    df_estadisticas = pd.DataFrame(zero_data, columns = ['metrica', 'valor', 'descripcion'])
+    
+    for i in range(0,len(metrica)):
+        df_estadisticas['metrica'][i] = metrica[i]
+        df_estadisticas['descripcion'][i] = descripcion[i]
+
+    df_estadisticas['valor'][0] = (param_data['rendimiento_log'].mean()-rf) / param_data['rendimiento_log'].std()
+    df_estadisticas['valor'][1] = 
+    df_estadisticas['valor'][2] = 
+    df_estadisticas['valor'][3] = 
+    df_estadisticas['valor'][4] = 
+    df_estadisticas['valor'][5] = 
+    df_estadisticas['valor'][6] = 
+    df_estadisticas['valor'][7] = 
     
--- principal.py
+++ principal.py
@@ -1,18 +1,17 @@
 # -- ------------------------------------------------------------------------------------ -- #
-
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
-
-# -- archivo: principal.py - datos generales para uso en el proyecto
-
-# -- mantiene: Francisco ME
-
-# -- repositorio: https://github.com/IFFranciscoME/LAB_2_JFME
-
+# -- archivo: principal.py - flujo principal del proyecto
+# -- mantiene: mauanaya
+# -- repositorio: https://github.com/mauanaya/LAB_2_VMAA
 # -- ------------------------------------------------------------------------------------ -- #
 
 import funciones as fn
+#%%
+datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
+fn.f_pip_size(param_ins='audusd')
+datos = fn.f_columnas_tiempo(param_data=datos)
+datos = fn.f_columnas_pips(param_data=datos)
+datos = fn.f_columnas_capital_acum(param_data=datos)
+df_1 = fn.f_estadisticas_ba(param_data=datos)
 
-datos = fn.f_leer_archivo(param_archivo='Hoja1.xlsx')
 
-#f_pip_size(param_ins='usdmxn')
-#datos = fn.f_columnas_datos(param_data=datos)
